3815d49acacf8fdcb6fcb0e13963a3fd45fccef7
Alexander Demidenko <mr.alexdemidenko@gmail.com>
2024-05-17 11:27:57 +0300

    fix: declare module

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
-export type gliderAutoplay = <T extends unknown>(
-    instance: T,
-    params: {
-        interval: number;
-        pausable?: boolean;
-        onPause?: () => void;
-        onRestart?: () => void;
-        startItem?: number;
-    }
-) => T & { pause: () => void; run: () => void; readonly isRunning: boolean };
+declare module "glider-autoplay" {
+    function gliderAutoplay<T extends unknown>(
+        instance: T,
+        params: {
+            interval: number;
+            pausable?: boolean;
+            onPause?: () => void;
+            onRestart?: () => void;
+            startItem?: number;
+        }
+    ): T & { pause: () => void; run: () => void; readonly isRunning: boolean };
+}
